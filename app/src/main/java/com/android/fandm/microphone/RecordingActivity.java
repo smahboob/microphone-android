7fecec6e33366af45cfc8c7e294a06ace1ebf397
Saad Mahboob <smahboob@fandm.edu>
2021-04-09 11:52:49 -0400

    Fully Working Microphone!

--- app/src/main/java/com/android/fandm/microphone/RecordingActivity.java
+++ app/src/main/java/com/android/fandm/microphone/RecordingActivity.java
@@ -5,19 +5,37 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.SystemClock;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.EnumMap;
+import java.util.Locale;
+import java.util.Objects;
 
 public class RecordingActivity extends AppCompatActivity {
 
@@ -26,10 +44,12 @@ public class RecordingActivity extends AppCompatActivity {
     MediaRecorder mediaRecorder;
     private boolean recording = false;
     public static final int REQUEST_AUDIO_PERMISSION_CODE = 1;
-    private static String fileName = null;
+    private String recordFile;
+    private RecyclerView audio_list_recycler_view;
+    private ArrayList<File> audio_files;
+    private RecordingAdapter recordingAdapter;
+    private MediaPlayer mediaPlayer = null;
 
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     @SuppressLint("ResourceType")
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,19 +57,32 @@ public class RecordingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recording);
         chronometer = findViewById(R.id.timerChronometer);
         recording_button = findViewById(R.id.recordingButton);
-        mediaRecorder = new MediaRecorder();
-        fileName = Environment.getDataDirectory().getAbsolutePath() + "/AudioRecording.3gp";
+        audio_list_recycler_view = findViewById(R.id.audioFilesRecycler);
+
+        updateRecyclerView();
 
         recording_button.setOnClickListener(v -> {
             updateRecordingStatus();
         });
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    @SuppressLint("ResourceType")
+    private void updateRecyclerView() {
+        String path = getExternalFilesDir("/").getAbsolutePath();
+        File directory = new File(path);
+        audio_files = new ArrayList<>(Arrays.asList(Objects.requireNonNull(directory.listFiles())));
+
+        recordingAdapter = new RecordingAdapter(audio_files);
+        audio_list_recycler_view.setAdapter(recordingAdapter);
+        audio_list_recycler_view.setLayoutManager(new LinearLayoutManager(this));
+        audio_list_recycler_view.setHasFixedSize(true);
+    }
+
+    @SuppressLint({"ResourceType", "NewApi"})
     private void updateRecordingStatus() {
         if(!recording) {
             if(permissionsGranted()) {
+                startRecording();
+                chronometer.setBase(SystemClock.elapsedRealtime());
                 chronometer.start();
                 recording_button.setImageTintList(ColorStateList.valueOf(getResources().getColor(R.color.on)));
                 recording_button.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.onBackground)));
@@ -60,11 +93,49 @@ public class RecordingActivity extends AppCompatActivity {
             }
         }
         else{
-            chronometer.stop();
-            recording_button.setImageTintList(ColorStateList.valueOf(getResources().getColor(R.color.off)));
-            recording_button.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.offBackground)));
-            recording = false;
+            if(permissionsGranted()){
+                chronometer.setBase(SystemClock.elapsedRealtime());
+                stopRecording();
+                chronometer.stop();
+                recording_button.setImageTintList(ColorStateList.valueOf(getResources().getColor(R.color.off)));
+                recording_button.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.offBackground)));
+                recording = false;
+            }
         }
+    }
+
+    private void startRecording() {
+        SimpleDateFormat dataFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.US);
+
+        Date now = new Date();
+        String recordPath = getExternalFilesDir("/").getAbsolutePath();
+        recordFile = "Audio Recording - " + dataFormat.format(now) + ".3gp";
+
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setOutputFile(recordPath + "/" + recordFile);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mediaRecorder.setMaxDuration(-1); //Duration unlimited or use RECORD_MAX_DURATION
+
+        try {
+            mediaRecorder.prepare();
+        } catch (IllegalStateException | IOException ise) {
+            Log.d("TAG",ise.toString());
+            Snackbar.make(findViewById(R.id.recordingActivity),"Failed to record!",Snackbar.LENGTH_SHORT).show();
+        }
+        mediaRecorder.start();
+        Log.d("TAG:", "Recorded in "+ recordPath + "/" + recordFile);
+        Snackbar.make(findViewById(R.id.recordingActivity),"Recording Started!",Snackbar.LENGTH_SHORT).show();
+
+    }
+
+    private void stopRecording() {
+        mediaRecorder.stop();
+        mediaRecorder.release();
+        mediaRecorder = null; //because we initialize again when hit record
+        Snackbar.make(findViewById(R.id.recordingActivity),"Recording Stopped!",Snackbar.LENGTH_SHORT).show();
+        updateRecyclerView();
     }
 
     //check to see if already have permission or not
@@ -86,6 +157,9 @@ public class RecordingActivity extends AppCompatActivity {
             if (grantResults.length > 0) {
                 boolean permissionToRecord = grantResults[0] == PackageManager.PERMISSION_GRANTED;
                 boolean permissionToStore = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                Log.d("TAG : ", String.valueOf(grantResults[0]));
+                Log.d("TAG : ", String.valueOf(grantResults[1]));
+
                 if (permissionToRecord && permissionToStore) {
                     Snackbar.make(findViewById(R.id.recordingActivity), "Permission Granted!", Snackbar.LENGTH_LONG).show();
                     updateRecordingStatus();
--- app/src/main/java/com/android/fandm/microphone/RecordingAdapter.java
+++ app/src/main/java/com/android/fandm/microphone/RecordingAdapter.java
@@ -1,67 +1,139 @@
 package com.android.fandm.microphone;
 
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 //adapter takes data and puts into view holder
 public class RecordingAdapter extends RecyclerView.Adapter<RecordingAdapter.ViewHolder>{
 
+    //private final File[] audio_files;
+    private final ArrayList<File> audio_files;
 
-    public RecordingAdapter(List<String> items) {
-        //this.items = items;
+    public RecordingAdapter(ArrayList<File> audio_files) {
+        this.audio_files = audio_files;
     }
 
+    public void removeItem(int position) {
+        audio_files.get(position).delete();
+        audio_files.remove(position);
+        notifyItemRemoved(position);
+    }
 
     @Override
     public void onBindViewHolder(@NonNull RecordingAdapter.ViewHolder holder, int position) {
-
+        File file = audio_files.get(position);
+        try {
+            holder.bind(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Button delete = holder.deleteButton;
+        delete.setOnClickListener(v -> {
+            removeItem(position);
+        });
     }
-
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         //use layout inflater to inflate a view
-        View todoView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recording_card, parent, false);
-        return new ViewHolder(todoView);
+        View recordingCardView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recording_card, parent, false);
+        return new ViewHolder(recordingCardView);
     }
 
-
-    //how many items are in the list
     @Override
     public int getItemCount() {
-        return 0;
+        return audio_files.size();
     }
 
 
 
     //new class
     //container to provide easy access to view that represent each row of the list
-    static class ViewHolder extends RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder{
 
         TextView fileName;
-        Button play;
-        Button stop;
-        Button delete;
+        Button playButton;
+        Button stopButton;
+        Button deleteButton;
+        private MediaPlayer mediaPlayer = null;
+        private boolean playing = false;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             fileName = itemView.findViewById(R.id.fileName);
-            play = itemView.findViewById(R.id.playButton);
-            stop = itemView.findViewById(R.id.stopButton);
-            delete = itemView.findViewById(R.id.deleteButton);
+            playButton = itemView.findViewById(R.id.playButton);
+            stopButton = itemView.findViewById(R.id.stopButton);
+            deleteButton = itemView.findViewById(R.id.deleteButton);
         }
 
         //update the view inside of the view holder with the data
-        public void bind(String item) {
+        public void bind(File file) throws FileNotFoundException {
+            String name = file.getName();
+            fileName.setText(name);
 
+            File tempFile = new File(file.getPath());
+            FileInputStream fis = new FileInputStream(tempFile);
+
+            playButton.setOnClickListener(v -> {
+               initializeMediaPlayer(fis);
+            });
+
+            stopButton.setOnClickListener(v -> {
+                if(playing){
+                    mediaPlayer.release();
+                    mediaPlayer = null;
+                }
+            });
+
+            deleteButton.setOnClickListener(v -> {
+                file.delete();
+
+            });
+        }
+
+        private void initializeMediaPlayer(FileInputStream fileInputStream) {
+            if(mediaPlayer == null)
+                mediaPlayer = new MediaPlayer();
+            try {
+                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                mediaPlayer.reset();
+                mediaPlayer.setDataSource(fileInputStream.getFD());
+                mediaPlayer.prepare();
+            } catch (Exception e) {
+                Log.d("TAG",e.toString());
+            }
+            mediaPlayer.setOnPreparedListener(mp -> {
+                mediaPlayer.start();
+                playing = true;
+            });
+            mediaPlayer.setOnCompletionListener(mp -> {
+                mediaPlayer.release();
+                mediaPlayer = null;
+                playing = false;
+            });
         }
     }
 }
